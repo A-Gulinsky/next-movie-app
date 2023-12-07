064a2ebb3eaa4a59fec7b7f9c4eac83676ff5947
Alexandr Gulinsky <swith0711@gmail.com>
2023-12-07 22:52:38 +0200

    fix metadata

--- app/page.jsx
+++ app/page.jsx
@@ -2,6 +2,7 @@ import { Trends } from '@/components/home/Trends';
 import { Suspense } from 'react';
 
 export const metadata = {
+  metadataBase: new URL(`https://next-movie-app-seven.vercel.app/`),
   title: 'Home',
   description: 'Description | Home Page',
   openGraph: {
@@ -10,7 +11,14 @@ export const metadata = {
     siteName: 'Movies Service',
     type: 'article',
     url: 'https://next-movie-app-seven.vercel.app/',
-    images: 'https://img.freepik.com/free-photo/fresh-natural-green-schefflera-arboricola-background_53876-153311.jpg?w=996&t=st=1701474143~exp=1701474743~hmac=50682caf3fdfd630dff9ea401437b10bd57f30234e16b1f53a14a718a5ea0498'
+    images: [
+      {
+        url: 'https://img.freepik.com/free-photo/fresh-natural-green-schefflera-arboricola-background_53876-153311.jpg?w=996&t=st=1701474143~exp=1701474743~hmac=50682caf3fdfd630dff9ea401437b10bd57f30234e16b1f53a14a718a5ea0498',
+        width: 1000,
+        height: 1000,
+        alt: 'Home Page'
+      }
+    ]
   }
 }
 
