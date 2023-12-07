357ab726b5fdfa4df20c3aad029d7a975ad44a9c
Alexandr Gulinsky <swith0711@gmail.com>
2023-12-07 22:59:27 +0200

    movies page metadata

--- app/movies/page.jsx
+++ app/movies/page.jsx
@@ -2,7 +2,22 @@ import { SearchMovies } from "@/components/movies/searchMovies/searchMovies"
 
 export const metadata = {
   title: 'Movies',
-  description: 'Description | Best Movies Page',
+  description: 'Description | Movies Page',
+  openGraph: {
+    title: 'Movies',
+    description: 'More than 100500 movies on our service',
+    siteName: 'Movies Service',
+    type: 'article',
+    url: 'https://next-movie-app-seven.vercel.app/movies',
+    images: [
+      {
+        url: 'https://img.freepik.com/free-vector/film-strip-background-with-clapper-board_1017-33456.jpg?w=1380&t=st=1701982718~exp=1701983318~hmac=8b9e520cd8980679f9bf06dcd3fcfb633b8b8157f824429cb8c51a1e3f36a6cd',
+        width: 1000,
+        height: 1000,
+        alt: 'Movies Page'
+      }
+    ]
+  }
 }
 
 export default function Movies() {
